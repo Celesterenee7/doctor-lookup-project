9e022d59a38a5a1aefee019f494485796a871e82
Celeste Morton <creneemorton@gmail.com>
2019-11-22 11:30:16 -0800

    begin the process of planning out the FE

--- src/backend.js
+++ src/backend.js
@@ -2,6 +2,7 @@ export class BetterDoctor {
     async searchByMedicalSubject(medicalSubject) {
         try {
             let respsone = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561%2C-122.655&sort=distance-asc&limit=20&user_key=${process.env.API_KEY}`);
+            console.log(response);
             let jsonifiedResponse = await response.json();
             return jsonifiedResponse;
         } catch (error) {
--- src/main.js
+++ src/main.js
@@ -5,3 +5,25 @@ import './styles.css';
 import {
     BetterDoctor
 } from './../src/backend.js';
+
+$(document).ready(function () {
+    $(".inputForm").submit(function (event) {
+        event.preventDefault();
+        let medicalSubject = "";
+
+        (async () => {
+            let betterDoctor = new BetterDoctor();
+            let response = await betterDoctor.searchByMedicalSubject(medicalSubject);
+            console.log(response);
+            getElements(response);
+        })();
+
+        function getElements(response) {
+            
+        }
+
+    });
+
+
+
+});

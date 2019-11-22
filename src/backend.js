329915b7c1fd26287fb8dd32f79e0ec8e3dfba1e
Celeste Morton <creneemorton@gmail.com>
2019-11-22 11:46:46 -0800

    verify that the api call is working

--- src/backend.js
+++ src/backend.js
@@ -1,7 +1,7 @@
 export class BetterDoctor {
     async searchByMedicalSubject(medicalSubject) {
         try {
-            let respsone = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561%2C-122.655&sort=distance-asc&limit=20&user_key=${process.env.API_KEY}`);
+            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561%2C-122.655%2C10&user_location=45.523%2C-122.676&sort=distance-asc&limit=10&user_key=${process.env.API_KEY}`);
             console.log(response);
             let jsonifiedResponse = await response.json();
             return jsonifiedResponse;

0dc81682a883e0cba5f5b47bfce24a4c9b7f68e5
Celeste Morton <creneemorton@gmail.com>
2019-11-22 10:56:36 -0800

    add the try and catch to the backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,9 +1,11 @@
-// export class BetterDoctor {
-//     async searchByMedicalSubject(medicalSubject) {
-//         try {
-//             let respsone = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561,-122.655,10&sort=distance-asc&limit=20&user_key=${process.env.API_KEY}`);
-//             let jsonifiedResponse = await response.json();
-//             return jsonifiedResponse;
-//         }
-//     }
-// }
+export class BetterDoctor {
+    async searchByMedicalSubject(medicalSubject) {
+        try {
+            let respsone = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561%2C-122.655&sort=distance-asc&limit=20&user_key=${process.env.API_KEY}`);
+            let jsonifiedResponse = await response.json();
+            return jsonifiedResponse;
+        } catch (error) {
+            console.log("There was an error with your submission: " + error.message);
+        }
+    }
+}

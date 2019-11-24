5e2fa1af0c35016a8e65a498786fafb5cfb84fde
Celeste Morton <creneemorton@gmail.com>
2019-11-24 10:27:33 -0800

    add changes to the FE and update type of search input form

--- src/backend.js
+++ src/backend.js
@@ -1,18 +1,7 @@
 export class BetterDoctor {
-    async searchByMedicalSubject(medicalSubject) {
+    async findDoctors(search) {
         try {
-            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalSubject}&location=45.561%2C-122.655%2C10&user_location=45.523%2C-122.676&sort=distance-asc&limit=10&user_key=${process.env.API_KEY}`);
-            console.log(response);
-            let jsonifiedResponse = await response.json();
-            return jsonifiedResponse;
-        } catch (error) {
-            console.log("There was an error with your submission: " + error.message);
-        }
-    }
-
-    async searchByDoctorName(doctorName) {
-        try {
-            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=45.561%2C-122.655%2C10&user_location=45.523%2C-122.676&sort=distance-asc&limit=10&user_key=${process.env.API_KEY}`);
+            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&location=OR&limit=10&user&query=` + search);
             console.log(response);
             let jsonifiedResponse = await response.json();
             return jsonifiedResponse;
--- src/main.js
+++ src/main.js
@@ -4,29 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {
     BetterDoctor
-} from './../src/backend.js';
-
+} from './backend.js';
 
 
 $(document).ready(function () {
-    $("form#inputForm").submit(function (event) {
-        const userSearch = $("#userSearch").val();
+    $("#docSearch").submit(function (event) {
+        const search = $("#userSearch").val();
         event.preventDefault();
-
         (async () => {
-            let newBetterDoctor = new BetterDoctor(userSearch);
-            if ($("#selectOptions").val() === "Medical Issue") {
-                let response = await newBetterDoctor.searchByMedicalSubject();
-                console.log(response);
-            } else if ($("#selectOptions").val() === "Doctors Name") {
-                let response = await newBetterDoctor.searchByDoctorName();
-                console.log(response);
-            } else if ($("#selectOptions").val() === "...") {
-                return "Please select an option";
-            }
-            // getElements(response);
-
+            let betterDoctor = new BetterDoctor();
+            const response = await betterDoctor.findDoctors(search);
+            getElements(response);
         })();
     });
 
+    const getElements = (response) => {
+        $("#doctorResults").text("");
+        if (response.data[0]) {
+            response.data.forEach(function (doctor) {
+                $("#doctorResults").prepend(`<div class="container">
+                <p class="doc-name">${doctor.profile.first_name} ${doctor.profile.last_name} <span class="doc-title">${doctor.profile.title}</span></p>
+                <p></p>
+                </div>`);
+                console.log(doctor.profile);
+            });
+        }
+    };
 });

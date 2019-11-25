a0186ee6991d027d6ff7ff6ca599ba84df21215c
Celeste Morton <creneemorton@gmail.com>
2019-11-25 12:44:15 -0800

    add additional info to the doc profile

--- src/main.js
+++ src/main.js
@@ -19,14 +19,31 @@ $(document).ready(function () {
     });
 
     const getElements = (response) => {
-        $("#doctorResults").text("");
+        $("#doctorResults").text();
         if (response.data[0]) {
             response.data.forEach(function (doctor) {
-                $("#doctorResults").prepend(`<div class="container">
-                <p class="doc-name">${doctor.profile.first_name} ${doctor.profile.last_name} <span class="doc-title">${doctor.profile.title}</span></p>
-                <p></p>
-                </div>`);
-                console.log(doctor.profile);
+                $("#doctorResults").prepend(`
+            <div class="container">
+            <p class="doc-name">${doctor.profile.first_name} ${doctor.profile.last_name} <span class="doc-title">${doctor.profile.title}</span></p>
+            <p><span class="facts">About: </span>${doctor.profile.bio}</p>
+            <p  class="${doctor.npi}"></p>
+            <p class="${doctor.profile.first_name}${doctor.profile.last_name}"></p>
+              </div>`);
+
+                doctor.practices.forEach(function (practice) {
+                    if (practice.accepts_new_patients === true) {
+                        practice.accepts_new_patients = "Yes, we are currently accepting new patients";
+                    } else if (practice.accepts_new_patients === false) {
+                        practice.accepts_new_patients = "Not at this time";
+                    }
+                    $("." + doctor.profile.first_name + doctor.profile.last_name).prepend(`
+                 <p><span class="facts">Location: </span> ${practice.visit_address.street} ${practice.visit_address.city}, ${practice.visit_address.state} ${practice.visit_address.zip}<br>
+                 <p><span class="facts">Phone: </span>${practice.phone}</p>
+                 <p class="${practice.uid}"></p>
+                 <p><span class="facts">Is the Doctor Accepting New Patients:</span> ${practice.accepts_new_patients}</p><hr>`);
+                    console.log(doctor.practices);
+
+                });
             });
         }
     };
